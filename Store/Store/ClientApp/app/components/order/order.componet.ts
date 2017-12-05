bc1d2494830171cd5a54145842aed1e843200ecf
Nelly Cheboi <nellycheboi12@augustana.edu>
2017-12-05 08:26:49 -0600

    :tada: Wired back end to the front end successfully

---
+++ Store/Store/ClientApp/app/components/order/order.componet.ts
@@ -0,0 +1,28 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+
+@Component({
+    selector: 'order',
+    templateUrl: './order.component.html',
+    styleUrls: ['./order.component.css'],
+
+})
+export class OrderComponent implements OnInit {
+
+    
+    // Inject HttpClient. 
+    // angular requires injection into components to be public
+    constructor(public http: HttpClient) { }
+
+    ngOnInit() {
+
+    }
+
+
+}
+
+interface User {
+    firstName: string;
+    lastName: string;
+}
---
+++ Store/Store/ClientApp/app/components/user/user.component.ts
@@ -0,0 +1,33 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+
+@Component({
+    selector: 'user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css'],
+
+})
+export class UserComponent implements OnInit {
+    public users: User;
+
+    // Injecting HttpClient. 
+    // angular requires injection into components to be public
+    constructor(public http: HttpClient) { }
+
+    // The response body doesn't return all the data you may need. Sometimes servers return special headers or status codes to indicate certain conditions, and inspecting those can be necessary. To do this, you can tell HttpClient you want the full response instead of just the body with the observe option:
+    // https://angular.io/guide/http#making-a-request-for-json-data
+    ngOnInit(): void {
+        this.http.get<User>('/api/users', { observe: 'response' }).subscribe(result => {
+            // Read the result field from the JSON response.
+            this.users = result.body;
+        }, error => console.error(error));
+     
+       
+    }
+}
+
+interface User {
+    firstName: string;
+    lastName: string;
+}
